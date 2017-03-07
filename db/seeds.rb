92ad18d448aa8087b7c5b5b94171e712654b6975
Thomas Ollivier <thomasollivierfrance@gmail.com>
2017-03-07 17:26:56 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# Creating 100 fake restaurants
+10.times do
+  article = Article.new(
+    title:    Faker::Zelda.game,
+    content:  Faker::Zelda.character
+
+  )
+  article.save!
+end
